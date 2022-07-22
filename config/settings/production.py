d8b8f929472ca1ef68d25b4ab77852b1f2c280bf
sowangchuk <108423011+sowangchuk@users.noreply.github.com>
2022-07-22 16:44:42 +0600

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -123,15 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 django_heroku.settings(locals())
 #Crispy templates for form rendering
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
